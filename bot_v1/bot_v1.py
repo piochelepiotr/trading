e9beed0bae196e49eb422622073d2b51f5f612e1
Piotr Wolski <piotr.wolski@telecom-paristech.fr>
2017-09-10 11:25:13 +0200

    adding lauch file for windows

--- bot_v1/bot_v1.py
+++ bot_v1/bot_v1.py
@@ -13,22 +13,11 @@ current_prices = manager.dict()
 ns.c = 0
 names = {7: 'BTC_BCN', 8: 'BTC_BELA', 10: 'BTC_BLK', 12: 'BTC_BTCD', 13: 'BTC_BTM', 14: 'BTC_BTS', 15: 'BTC_BURST', 20: 'BTC_CLAM', 24: 'BTC_DASH', 25: 'BTC_DGB', 27: 'BTC_DOGE', 28: 'BTC_EMC2', 31: 'BTC_FLDC', 32: 'BTC_FLO', 38: 'BTC_GAME', 40: 'BTC_GRC', 43: 'BTC_HUC', 50: 'BTC_LTC', 51: 'BTC_MAID', 58: 'BTC_OMNI', 60: 'BTC_NAUT', 61: 'BTC_NAV', 63: 'BTC_NEOS', 64: 'BTC_NMC', 66: 'BTC_NOTE', 69: 'BTC_NXT', 73: 'BTC_PINK', 74: 'BTC_POT', 75: 'BTC_PPC', 83: 'BTC_RIC', 86: 'BTC_SJCX', 89: 'BTC_STR', 92: 'BTC_SYS', 97: 'BTC_VIA', 98: 'BTC_XVC', 99: 'BTC_VRC', 100: 'BTC_VTC', 104: 'BTC_XBC', 108: 'BTC_XCP', 112: 'BTC_XEM', 114: 'BTC_XMR', 116: 'BTC_XPM', 117: 'BTC_XRP', 121: 'USDT_BTC', 122: 'USDT_DASH', 123: 'USDT_LTC', 124: 'USDT_NXT', 125: 'USDT_STR', 126: 'USDT_XMR', 127: 'USDT_XRP', 129: 'XMR_BCN', 130: 'XMR_BLK', 131: 'XMR_BTCD', 132: 'XMR_DASH', 137: 'XMR_LTC', 138: 'XMR_MAID', 140: 'XMR_NXT', 148: 'BTC_ETH', 149: 'USDT_ETH', 150: 'BTC_SC', 151: 'BTC_BCY', 153: 'BTC_EXP', 155: 'BTC_FCT', 158: 'BTC_RADS', 160: 'BTC_AMP', 162: 'BTC_DCR', 163: 'BTC_LSK', 166: 'ETH_LSK', 167: 'BTC_LBC', 168: 'BTC_STEEM', 169: 'ETH_STEEM', 170: 'BTC_SBD', 171: 'BTC_ETC', 172: 'ETH_ETC', 173: 'USDT_ETC', 174: 'BTC_REP', 175: 'USDT_REP', 176: 'ETH_REP', 177: 'BTC_ARDR', 178: 'BTC_ZEC', 179: 'ETH_ZEC', 180: 'USDT_ZEC', 181: 'XMR_ZEC', 182: 'BTC_STRAT', 183: 'BTC_NXC', 184: 'BTC_PASC', 185: 'BTC_GNT', 186: 'ETH_GNT', 187: 'BTC_GNO', 188: 'ETH_GNO'}
 
-def load_data():
-    files = os.listdir(folder)
-    files = sorted(files)
-    for filename in files:
-        name = filename[:len(filename)-4]
-        print("loading",name)
-        current_prices[name] = 0
-
 def init():
-    #refresh_data.refresh_data()
-    load_data()
-    choose_buy_points()
     print("loaded")
     multiprocessing.Process(target=wamp_connect,args=()).start()
     print("launch commands")
-    commands()
+    main_loop()
 
 def on_message(ws, message):
     try:
@@ -106,7 +95,7 @@ def load_config():
         if config[name]["buy_price"] > config[name]["signal_buy_price"]:
             print("buy price > signal buy")
             exit()
-        if config[name]["signal_buy"] > config[name]["sell_price"]:
+        if config[name]["signal_buy_price"] > config[name]["sell_price"]:
             print("signal buy > sell price")
             exit()
     return config
@@ -126,7 +115,7 @@ def main_loop():
     buy_orders = {}
     sell_orders = {}
     i = 0
-    number_moneys = 2
+    number_moneys = 100
     balances = {}
     while True:
         #recherche de stop loss, vend si un seuil de sécurité est dépassé
@@ -164,7 +153,9 @@ def main_loop():
                         del buy_orders[orderNumber]
         #regarder les monnaies à acheter
         for name in list(config):
-            if balances[name] == 0 and not name in [name for name,expiration_date in buy_orders]):
+            if (len(balances) == 0 or balances[name] == 0) and not name in [name for name,expiration_date in buy_orders]:
+                if not name in list(current_prices):
+                    continue
                 if current_prices[name] < config[name]["signal_buy_price"]:
                     balances = trading_api.pol.returnBalances()
                     btc_amount,btc_equivalent = get_btc_equivalent(balances)
@@ -175,6 +166,4 @@ def main_loop():
         i += 1
         time.sleep(1)
 
-if __name__ == "__main__":
-    #init()
-    main_loop()
+init()
