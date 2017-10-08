3a358f6dcf2450f3477f7ca281e5ee62753dd09d
Piotr Wolski <piotr.wolski@telecom-paristech.fr>
2017-10-08 16:45:41 +0200

    removing useless files and functions

--- strategies/load_data.py
+++ strategies/load_data.py
@@ -17,46 +17,6 @@ def load_money(period,name):
     index_to_keep = list(range(len(data)-number_points,len(data)))
     data = data.take(index_to_keep)
     data = data.reset_index()
-    data['min'] = indicators.price_min(data['close'],(3600*24*2)//period)
-    #data['rsi'] = indicators.rsi(data['close'],14)
-    #data['average'] = indicators.average(data['close'],800)
-    #c = compute_stats.compute_buy_line(data,len(data["close"]))
-    #c_L = compute_stats.compute_buy_lines(data)
-    #data['average_min'] = [d*c for i,d in enumerate(data["average"])]
-    #data['average_min'] = [d*0.96 for d in data["average"]]
-    #data['average_min2'] = [d*0.90 for d in data["average"]]
-    #data['average_min3'] = [d*0.8 for d in data["average"]]
-    #data['change_24h'] = indicators.change(data['open'],(3600*24*3)//period)
-    #macd,signal_line = indicators.macd(data['open'],30,80,80)
-    #data['time_macd'] = indicators.time_macd(macd,signal_line)
-    #data['macd'] = macd
-    #data['signal_line'] = signal_line
-    #macd_short,signal_line_short = indicators.macd(data['open'],3,8,8)
-    #data['macd_short'] = macd_short
-    #data['signal_line_short'] = signal_line_short
-    #compute_stats.compute_move(data,3,3)
-    #compute_stats.stat_up(data)
-    #for i in range(9):
-    #    compute_stats.stat_up_rsi(data,10*i,10*(i+1))
-    #for i in range(-3,3):
-    #    compute_stats.stat_up_change24h(data,10*i,10*(i+1))
-    #compute_stats.stat_up_macd(data)
-    #add indicators
-    #down_line,middle_line,up_line = indicators.bollinger_bands(data['open'],80,3)
-    #data['bollinger_up'] = up_line
-    #data['bollinger_down'] = down_line
-    #data['bollinger_middle'] = middle_line
-    #data['change_24h'] = indicators.change(data['close'],(3600)//period)
-    #data['min_week'] =  indicators.price_min(data['close'],(3600*24*7)//period)
-    #data['min_month'] = indicators.price_min(data['close'],(3600*24*30)//period)
-    #data['max_week'] =  indicators.price_max(data['close'],(3600*24*7)//period)
-    #data["long_term"] = indicators.ema(data["close"],100)
-    #data["long_term2"] = indicators.ema(data["close"],90)
-    #data["long_term3"] = indicators.ema(data["close"],80)
-    #data["long_term4"] = indicators.ema(data["close"],20)
-    #data["short_term"] = indicators.ema(data["close"],100)
-    #data['percent_line'] = indicators.percent_line(data['close'],10,(3600*24*2)//period)
-    #data['percent_line_2'] = indicators.percent_line(data['close'],10,(3600*2)//period)
     return data
 
 def load_moneys(period):
--- strategies/trading.py
+++ strategies/trading.py
@@ -11,23 +11,13 @@ def plot(data,name,buy_points_x,buy_points_y,sell_points_x,sell_points_y):
     n = len(data['close'])
     fig,ax = plt.subplots(3,sharex=True)
     candlestick2_ohlc(ax[0],data['open'],data['high'],data['low'],data['close'],width=0.6)
-    ax[0].plot(buy_points_x,buy_points_y,'go',color='#00ff00')
-    ax[0].plot(sell_points_x,sell_points_y,'go',color='red')
     ax[1].plot(data['btc_equ'])
     ax[0].set_title(name)
     plt.show()
-
-def test_trade(period,name):
-    data = load_data.load_money(period,name)
-    plot(data,name)
-
-#for name in load_data.currency_list:
-#    test_trade(900,name)
 
 period = 300
 moneys = load_data.load_moneys(period)
 buy_points_x,buy_points_y,sell_points_x,sell_points_y = decisions.manage_portfolio(moneys,(2*3600)//period)
 for name in moneys:
-    #buy_points_x,buy_points_y,sell_points_x,sell_points_y = compute_stats.market_maker(moneys[name])#compute_stats.stat_up_macd(moneys[name])
     plot(moneys[name],name,buy_points_x[name],buy_points_y[name],sell_points_x[name],sell_points_y[name])
 
